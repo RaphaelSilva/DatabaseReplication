ef2eafb41e85da1eaf8f1d25f0ac1deed1b7fdce
raphaelsilva <eng.raphaelsn@gmail.com>
2026-01-31 09:15:38 -0300

    feat: Add root directory check, dynamically load IPs from Terraform for Ansible inventory, and refine script navigation.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,28 +1,43 @@
 #!/bin/bash
+#verify if the script is run from the root directory
+if [ "$(basename "$(pwd)")" != "DatabaseReplication" ]; then
+    echo "Please run the script from the root directory"
+    exit 1
+fi
 
+echo "Starting the script..."
 # Load environment variables from parent directory
-if [ -f ../.env ]; then
-    export $(cat ../.env | grep -v '^#' | xargs)
+if [ -f .env ]; then
+    export $(cat .env | grep -v '^#' | xargs)
+fi
+
+# Load IPs from terraform.tfvars
+TF_VARS="./terraform/terraform.tfvars"
+if [ -f "$TF_VARS" ]; then
+    echo "Loading IPs from $TF_VARS..."
+    # Extract all IPs that are not commented out
+    ALL_IPS=$(grep '^[[:space:]]*ip[[:space:]]*=[[:space:]]*"' "$TF_VARS" | cut -d'"' -f2 | cut -d'/' -f1)
+
+    PRIMARY_IP=$(echo "$ALL_IPS" | head -n 1)
+    REPLICAS_IPS=$(echo "$ALL_IPS" | tail -n +2)
 fi
 
 # 1. Create Container in Proxmox
 echo "Creating Container in Proxmox..."
-cd ../terraform
+cd ./terraform
 terraform init && terraform apply -auto-approve
-
-# 2. Wait for the container to boot up
+cd ../
 sleep 10
 
-# 3. Configure the Database
-cd ../ansible
+# 2. Configure the Database
+cd ./ansible
 echo "Configuring the Database..."
 cat <<EOF > inventory.ini
 [primary]
 $PRIMARY_IP
 
 [replicas]
-$REPLICA_1_IP
-$REPLICA_2_IP
+$REPLICAS_IPS
 
 [postgres_all:children]
 primary
